277b3c93cbbfdad1b5c38dd28189be268bee22e8
Jeremy Jany <jany@mathematik.tu-darmstadt.de>
2022-06-13 11:41:21 +0200

    implemented method readAll in csvReader

--- src/main/java/de/exxcellent/challenge/csvReader.java
+++ src/main/java/de/exxcellent/challenge/csvReader.java
@@ -1,11 +1,27 @@
 package de.exxcellent.challenge;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class csvReader implements Reader{
 
     @Override
     public List<String[]> readAll(String filePath) {
-        return null;
+        List<String[]> allLines = new ArrayList<>();
+        String line;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filePath));
+            while ((line = br.readLine()) != null) {
+                allLines.add(line.split(","));
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return allLines;
     }
 }
--- src/test/java/de/exxcellent/challenge/csvReaderTest.java
+++ src/test/java/de/exxcellent/challenge/csvReaderTest.java
@@ -3,21 +3,28 @@ package de.exxcellent.challenge;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class csvReaderTest {
 
-    private String successLabel = "not successful";
+    private String filePath = "C:\\Users\\Jeremy\\IdeaProjects\\programming-challenge\\src\\main\\resources\\de\\exxcellent\\challenge\\weather.csv";
+    csvReader reader;
 
     @BeforeEach
     void setUp() {
-        successLabel = "successful";
+        reader = new csvReader();
     }
 
     @Test
     void readAllTest() {
-        assertEquals("successful", successLabel, "My expectations were not met");
+        List<String[]> allLines = reader.readAll(filePath);
+        String[] attributes = allLines.get(0);
+        List<String[]> values = allLines.subList(1, allLines.size());
+        assertEquals("Day", attributes[0]);
+        assertEquals(88, Integer.parseInt(values.get(0)[1]));
     }
 
 }
