692b4c41bec6d29b950b0d59be8bbccfb0ee06ab
Jeremy Jany <jany@mathematik.tu-darmstadt.de>
2022-06-13 10:36:33 +0200

    introduced interface Reader and class csvReader

---
+++ src/main/java/de/exxcellent/challenge/Reader.java
@@ -0,0 +1,9 @@
+package de.exxcellent.challenge;
+
+import java.util.List;
+
+public interface Reader {
+
+    List<String[]> readAll(String filePath);
+
+}
---
+++ src/main/java/de/exxcellent/challenge/csvReader.java
@@ -0,0 +1,11 @@
+package de.exxcellent.challenge;
+
+import java.util.List;
+
+public class csvReader implements Reader{
+
+    @Override
+    public List<String[]> readAll(String filePath) {
+        return null;
+    }
+}
