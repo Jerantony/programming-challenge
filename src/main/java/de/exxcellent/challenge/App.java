c859ad2c29a18d1651eab6731698c68c72dd41ac
Jeremy Jany <jany@mathematik.tu-darmstadt.de>
2022-06-14 13:04:42 +0200

    implemented App.java

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,5 +1,8 @@
 package de.exxcellent.challenge;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
@@ -14,12 +17,30 @@ public final class App {
      */
     public static void main(String... args) {
 
-        // Your preparation code …
+        DataBaseInteraction dbInteraction = new DataBaseInteraction();
+        csvReader reader = new csvReader();
 
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
+        List<String[]> weatherData = reader.readAll("C:\\Users\\Jeremy\\IdeaProjects\\programming-challenge\\src\\main\\resources\\de\\exxcellent\\challenge\\weather.csv");
+
+        String table = "weather";
+        String[] attributes = weatherData.get(0);
+        List<String[]> values = weatherData.subList(1, weatherData.size());
+
+        dbInteraction.clearTable(table);
+        dbInteraction.insertValues(table, attributes, values);
+        String dayWithSmallestTempSpread = dbInteraction.argMinAbsDiff(table, "Day", "MxT", "MnT");
         System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
 
-        String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
+
+        List<String[]> footballData = reader.readAll("C:\\Users\\Jeremy\\IdeaProjects\\programming-challenge\\src\\main\\resources\\de\\exxcellent\\challenge\\football.csv");
+
+        table = "football";
+        attributes = footballData.get(0);
+        values = footballData.subList(1, footballData.size());
+
+        dbInteraction.clearTable(table);
+        dbInteraction.insertValues(table, attributes, values);
+        String teamWithSmallestGoalSpread = dbInteraction.argMinAbsDiff(table, "Team", "Wins", "Losses");
         System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
     }
 }
