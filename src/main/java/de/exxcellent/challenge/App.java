9fb90a3e9a9af7e64c3105c6ba81d6e5f0c74914
Jeremy Jany <jany@mathematik.tu-darmstadt.de>
2022-06-14 18:07:12 +0200

    implemented method flattenList

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,7 +1,10 @@
 package de.exxcellent.challenge;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+
 import static java.util.Arrays.asList;
 
 /**
@@ -15,14 +18,33 @@ public final class App {
     public static final List<Object> EXAMPLE_NESTED_LIST = Collections.unmodifiableList(
             asList(1, asList(2, 3), asList(4, asList(5, 6)))
     );
+    public static final List<Object> EXAMPLE_NESTED_LIST_2 = Collections.unmodifiableList(
+            asList(asList("a", asList("b", "c")), "d", asList("e", "f"))
+    );
 
+    public static List<Object> flattenList(List<Object> nestedList){
+        return flattenListHelper(nestedList, new ArrayList<>());
+    }
+
+    public static List<Object> flattenListHelper(List<Object> nestedList, List<Object> log){
+        if (nestedList.isEmpty())
+                return log;
+
+        if (!(nestedList.get(0) instanceof List)){
+            log.add(nestedList.get(0));
+            return flattenListHelper(nestedList.subList(1, nestedList.size()), log);
+        }
+
+        log = flattenListHelper((List<Object>) nestedList.get(0), log);
+        return flattenListHelper(nestedList.subList(1,nestedList.size()), log);
+    }
 
     /**
      * This is the main entry method of your program.
      * @param args The CLI arguments passed
      */
     public static void main(String... args) {
-        List<Integer> result = asList(-1, -2); // yourFunction(EXAMPLE_NESTED_LIST)
+        List<Object> result = flattenList(EXAMPLE_NESTED_LIST); // yourFunction(EXAMPLE_NESTED_LIST)
 
         System.out.print(EXAMPLE_NESTED_LIST);
         System.out.print(" -> ");
--- src/test/java/de/exxcellent/challenge/AppTest.java
+++ src/test/java/de/exxcellent/challenge/AppTest.java
@@ -3,6 +3,9 @@ package de.exxcellent.challenge;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static java.util.Arrays.asList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
@@ -11,21 +14,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 class AppTest {
 
-    private String successLabel = "not successful";
-
-    @BeforeEach
-    void setUp() {
-        successLabel = "successful";
+    @Test
+    void flattenListTest() {
+        List<Object> flattenedList = App.flattenList(App.EXAMPLE_NESTED_LIST);
+        List<Integer> goal = asList(1, 2, 3, 4, 5, 6);
+        assertEquals(goal, flattenedList, "My expectations were not met");
     }
 
     @Test
-    void aPointlessTest() {
-        assertEquals("successful", successLabel, "My expectations were not met");
+    void flattenListTest2() {
+        List<Object> flattenedList = App.flattenList(App.EXAMPLE_NESTED_LIST_2);
+        List<String> goal = asList("a", "b", "c", "d", "e", "f");
+        assertEquals(goal, flattenedList, "My expectations were not met");
     }
 
     @Test
-    void runFootball() {
-        App.main("--football", "football.csv");
+    void runApp() {
+        App.main();
     }
-
 }
